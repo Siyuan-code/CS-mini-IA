691742df9915bce8862b36404796b3e52d558fbc
Siyuan-code <73175958+Siyuan-code@users.noreply.github.com>
2021-04-29 08:30:23 +0400

    Update findDE.php

--- findDE.php
+++ findDE.php
@@ -1,11 +1,37 @@
 <?php
+require 'link.php';
+
+$query = "SELECT * FROM studentinfo WHERE grade = 'D' OR grade ='F'";
+
 $result = mysqli_query($conn, $query);
 
-mysqli_free_result($result);
+$studentinfos = array();
 
-mysqli_close($conn);
-
-
-
+while($grade = mysqli_fetch_assoc($result)){
+    if($grade['grade'] == 'D' || $grade['grade'] == 'F'){
+        array_push($studentinfos[1], $grade['names']);
+        array_push($studentinfos[2], $grade['grade']);
+        array_push($studentinfos[3], $grade['EE_subject']);
+        array_push($studentinfos[4], $grade['email']);
+    }
+    // print_r($studentinfos);
+    echo $grade['names'];
+    // echo $grade['grade'];
+    // echo $grade['EE_subject'];
+    // echo $grade['email'];
+}
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="table.css?rnd=5">
+</head>
+<body>
+    <!-- <a href="home.php"><button class="back2">Back to homepage</button></a> -->
+</body>
+</html>
